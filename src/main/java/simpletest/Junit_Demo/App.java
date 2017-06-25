5016aef405882cd6dbf17b73add416aeac1a0d1b
Ahathavan <ahathavan@murugn.com>
2017-06-25 15:16:35 -0400

    first commit

---
+++ src/main/java/simpletest/Junit_Demo/App.java
@@ -0,0 +1,13 @@
+package simpletest.Junit_Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
